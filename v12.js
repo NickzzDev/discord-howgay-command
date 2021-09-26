386e1fe85c839da2a23142cd0ba7b222ad6491f3
Nickzz <85542797+NickzzDev@users.noreply.github.com>
2021-09-26 18:40:02 +1000

    Create v12.js

---
+++ v12.js
@@ -0,0 +1,18 @@
+
+const discord = require('discord.js')
+
+
+exports.run = async (client, message, args) => {
+    var member = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+    if(!member) return message.reply("Please Mention a User!!");
+
+    let gay = Math.floor(Math.random() * 101);
+
+    const howgayembed = new discord.MessageEmbed()
+    .setTitle('Gay Calculator')
+    .setDescription(`${member} is ${gay}% Gay 🌈\n\nCreated by [Nickzz](https://github.com/NickzzDev/)`)
+    .setColor("GREEN");
+
+    message.channel.send(howgayembed)
+  
+}
